f158126d2c6cd44077994e26735134597f74b9ae
Pinky Gautam <pkgtm1998@gmail.com>
2024-03-25 19:52:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,24 @@ const imgArr = [
     'wolf.png',
 ];
 
-let index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let index = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let game = [];
 let guess = [];
 let score = 0;
 
 function getImage() {
     let imgIndex = -1;
-    if(index.length>0)
-    {
-        imgIndex = index[Math.floor(Math.random()*index.length)];
-        
+    if (index.length > 0) {
+        imgIndex = index[Math.floor(Math.random() * index.length)];
+
     }
     return imgIndex;
 }
 function setUp() {
     let imgIndex = getImage();
-    while(imgIndex!=-1)
-    {
-        game.push(imgIndex%8);
-        index.splice(index.indexOf(imgIndex),1);
+    while (imgIndex != -1) {
+        game.push(imgIndex % 8);
+        index.splice(index.indexOf(imgIndex), 1);
         imgIndex = getImage();
 
     }
@@ -49,63 +47,65 @@ async function flip(target) {
 
 }
 
-function displayWin() {
-    const winmsg = document.querySelector('.message');
-    const header = document.querySelector('.header');
-    // console.log(winmsg);
-    winmsg.style.zIndex = 33;
-    winmsg.style.top = '50%';
-    winmsg.style.transition = 'all 1s';
-    header.innerHTML = 'Congratulations'
+function displayCover(message) {
+    const msg = document.querySelector('.message');
+    const cover = document.querySelector('.cover');
+    const game = document.querySelector('.game');
+    msg.innerHTML = message
+    cover.style.transition = 'display 4s ease-in-out';
+    cover.style.display = 'flex'
+    game.style.display = 'none'
 
 }
 function checkForMatch(target) {
-    if(guess.length===0){
+    if (guess.length === 0) {
         disableClick(target);
         guess.push(target);
     }
-    else if(guess.length%2===1) {
+    else if (guess.length % 2 === 1) {
         const last = guess.pop();
-        const lastImgIndex = game[last.id]%8;
-        const currentImgIndex = game[target.id]%8;
-        if(lastImgIndex!==currentImgIndex){
+        const lastImgIndex = game[last.id] % 8;
+        const currentImgIndex = game[target.id] % 8;
+        if (lastImgIndex !== currentImgIndex) {
             flip(last);
             flip(target);
-            }
-        else
-        {
-            if(last.id!== target.id)
-            {
+        }
+        else {
+            if (last.id !== target.id) {
                 score++;
                 console.log(score);
                 disableClick(last);
                 disableClick(target);
-                if(score===8)
-                {
-                    displayWin();
+                if (score === 8) {
+                    displayCover('You Win !!!');
                 }
             }
-            else{
+            else {
                 enableClick(target);
             }
         }
     }
-    else{
+    else {
         guess.push(target);
     }
 
 }
-setUp();
-const blocks = Array.from(document.querySelectorAll('.block'));
-// console.log(blocks);
-blocks.forEach((block)=>{
-    block.addEventListener('click',handleClick)
-})
+
+
+function render_game() {
+    setUp();
+    blocks.forEach((block) => {
+        block.addEventListener('click', handleClick)
+    })
+}
+
 function handleClick(event) {
-    const {target} = event;
+    const { target } = event;
     const imgUrl = imgArr[game[blocks.indexOf(target)]];
     target.style.backgroundImage = `url(img/${imgUrl})`;
     target.style.backgroundSize = 'cover';
     checkForMatch(target);
-    
 }
+
+const blocks = Array.from(document.querySelectorAll('.block'));
+render_game()
